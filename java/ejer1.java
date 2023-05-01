636572a4ef1b925a51c71d654e6cc7adb8a60a01
JorgeGonzalez11 <128486212+JorgeGonzalez11@users.noreply.github.com>
2023-05-01 19:12:16 +0200

    Add files via upload

---
+++ java/ejer1.java
@@ -0,0 +1,15 @@
+package ejercicios;
+
+public class ejer1 {
+
+	public static void main(String[] args) {
+		int anyo = 2023;
+		int anyoMas = anyo +1; 
+		
+		System.out.println("Buenas noches. En estos días tan especiales a finales del año " +anyo+ "en los que siempre nos deben unir\r\n"
+				+ "los mejores sentimientos os deseo, junto a la Reina y nuestras hijas, una Feliz Navidad y que en el\r\n"
+				+ "próximo año " +anyoMas+ "podáis ver cumplidos vuestros anhelos y aspiraciones");
+		
+	}
+
+}
---
+++ java/ejer2.java
@@ -0,0 +1,15 @@
+package ejercicios;
+
+public class ejer2 {
+
+	public static void main(String[] args) {
+		String nombre = "Jorge" ;
+		int edad = 19;
+		double altura = 1.83;
+		
+		System.out.println("Te llamas "+nombre+ ", tienes "+edad+ " años y mides "+altura+ " metros");
+		
+		
+	}
+
+}
